ddb2c35577a73f20ab8b9046fc77dd13ae6ddd50
Nayandin77 <nayandindennis@yahoo.com>
2020-01-30 21:37:52 -0500

    created basic setup for test-game

---
+++ test-game/js/main.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 700,
+    height: 550,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
